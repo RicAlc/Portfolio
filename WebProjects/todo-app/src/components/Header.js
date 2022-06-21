a6d5bdca2769c6ac44ff6686e4bbec0a33e165de
Ricardo Alcalá <alcala.ricardoc@gmail.com>
2022-06-21 01:43:50 -0500

    base

---
+++ WebProjects/todo-app/src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>HEADER</h1>
+      <img src="" alt="Boton tema oscuro / claro" />
+    </header>
+  );
+}
+export default Header;
