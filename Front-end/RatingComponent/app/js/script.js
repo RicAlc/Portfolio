138ee0b72a32552cd620879efb8f039948f9decd
RicAlc <alcala.ricardoc@gmail.com>
2022-06-25 15:55:04 -0500

    msg animation, and commented script

--- Front-end/RatingComponent/app/js/script.js
+++ Front-end/RatingComponent/app/js/script.js
@@ -1,8 +1,9 @@
 const sbmt_btn = document.getElementById("sbmt_btn");
 sbmt_btn.addEventListener("click", submit);
-var rting = 0;
 const user_score = document.querySelectorAll(".rating-btn");
+let rting;
 
+// Change score button state to active and unset the rest
 user_score.forEach((element) => {
   element.addEventListener("click", function rate() {
     for (var i = 0; i < user_score.length; i++) {
@@ -13,19 +14,20 @@ user_score.forEach((element) => {
   });
 });
 
-if (document.getElementById("st1").classList.contains(".hide")) {
-  console.log("it is hide");
-}
-
+// Set screen 2 with score selected
 function submit() {
-  if (rting > 0) {
+  if (!isNaN(rting)) {
     const st1 = document.getElementById("st1");
     const st2 = document.getElementById("st2");
     document.querySelector(".hide").style.cssText =
       "transform:translate(0, -100%)";
     console.log("Submitted");
     st1.classList.add("hide");
-    st2.classList.remove("hide");
+    setTimeout(() => {
+      st2.classList.remove("hide");
+      console.log("tiempo de espera");
+    }, 250);
+
     document.getElementById("score_value").innerHTML = `  ${rting} `;
   } else {
     window.alert("Selecciona un valor");
